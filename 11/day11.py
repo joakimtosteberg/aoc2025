acbb4b273072231df39eb589bbfcc9d553d200ca
Joakim Tosteberg <joakim@tosteberg.se>
2025-12-11 11:26:59 +0100

    day11 part2

--- 11/day11.py
+++ 11/day11.py
@@ -9,18 +9,34 @@ with open(sys.argv[1], "r") as f:
         outputs = output_str.strip().split(' ')
         connections[node] = outputs
 
-def dfs(connections, node, path, goal):
+def dfs(connections, node, goal, lookup, skip):
     if node == goal:
-        return [path]
+        return 1
 
     if not node in connections:
-        return []
+        return 0
 
-    paths = []
+    if node in lookup:
+        return lookup[node]
+
+    num_paths = 0
     for output in connections[node]:
-        paths += dfs(connections, output, path + [node], goal)
+        if output in skip:
+            continue
 
-    return paths
+        num_new_paths = dfs(connections, output, goal, lookup, skip)
+        lookup[output] = num_new_paths
+        num_paths += num_new_paths
 
-print(len(dfs(connections, 'you', list(), 'out')))
+    return num_paths
 
+print(f"part1: {dfs(connections, 'you', 'out', dict(), set())}")
+
+num_paths  = dfs(connections, 'svr', 'fft', dict(), {'dac','out'}) * dfs(connections, 'fft', 'dac', dict(), {'svr','out'}) * dfs(connections, 'dac', 'out', dict(), {'fft','svr'}) + dfs(connections, 'svr', 'dac', dict(), {'fft', 'out'}) * dfs(connections, 'dac', 'fft', dict(), {'svr', 'out'}) *  dfs(connections, 'fft', 'out', dict(), {'svr','dac'})
+print(f"part2: {num_paths}")
+
+
+
+
+#print(len(dfs(connections, 'svr', 'out', set(), {}, {'fft','dac'})))
+#print(len(dfs(connections, 'svr', list(), 'out')))
